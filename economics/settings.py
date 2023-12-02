0fc8f8281185b5fc3778e88a2a2a6f900ca4b0b7
Rapasi <ramierk@gmail.com>
2023-12-02 21:28:21 +0200

    Optimized performance for wind turbine map

--- economics/settings.py
+++ economics/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles/'
+STATIC_ROOT = '/home/ramie/Rami/web_app/production/Homesite/staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
